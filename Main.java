69c19a8bc038f2b6f15686344d2e09c363445562
Ketketpli <varelapaulo851@gmail.com>
2026-02-14 23:38:00 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,77 @@
+import entities.Employee;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+        boolean idExists;
+
+        System.out.print("How many employees will be registered? ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Employee #" + (i+1) + ": ");
+            System.out.print("Id: ");
+            int id = sc.nextInt();
+
+            do {
+                idExists = false;
+
+                for (Employee e : list) {
+                    if (e.getId() == id) {
+                        idExists = true;
+                        System.out.print("ID already exists, enter again: ");
+                        id = sc.nextInt();
+                        break;
+                    }
+                }
+            } while (idExists);
+
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Salary: ");
+            double salary = sc.nextDouble();
+            System.out.println();
+
+            Employee emp = new Employee(id, name, salary);
+            list.add(emp);
+        }
+
+        System.out.print("Enter the employee id that will have salary increase : ");
+        int id = sc.nextInt();
+        idExists = false;
+
+        for (Employee e : list) {
+
+            if (e.getId() == id) {
+                idExists = true;
+                System.out.print("Enter the percentage: ");
+                double increase = sc.nextDouble();
+                e.increaseSalary(increase);
+                break;
+            }
+        }
+
+        if (!idExists) {
+            System.out.println("This ID does not exist!");
+        }
+
+        System.out.println();
+        System.out.println("List of employees: ");
+        for (Employee e : list) {
+            System.out.println(e);
+        }
+
+        sc.close();
+    }
+}
